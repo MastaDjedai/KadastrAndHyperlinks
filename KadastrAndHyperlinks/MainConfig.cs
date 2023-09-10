ac5a55209b59b14c2473b5fcfc82e24f07c8d1fd
kievmastadjedai@gmail.com <kievmastadjedai@gmail.com>
2023-09-10 21:49:07 +0300

    Обновление выбора ячеек

--- KadastrAndHyperlinks/MainConfig.cs
+++ KadastrAndHyperlinks/MainConfig.cs
@@ -80,22 +80,22 @@ namespace KadastrAndHyperlinks
             string rootFolder = pathToFolder;
             var package = new ExcelPackage(xlsxPath);
             var worksheet = package.Workbook.Worksheets[0];
-            int column = worksheet.Dimension.Rows;
-            List<string>fullFolderPath = new List<string>();
-            for (int i = 1; i <= column; i++)
+            int rows = worksheet.Dimension.Rows;
+            List<string> fullFolderPath = new List<string>();
+            for (int i = 1; i <= rows; i++)
             {
                 string temporaryStorage = worksheet.Cells[i, 1].Text.Replace(":", "");
                 fullFolderPath.Add(Path.Combine(rootFolder, temporaryStorage));
             }
 
             string[] folders = Directory.GetDirectories(rootFolder);
-            for(int i = 1; i <= column; i++)
+            for (int i = 1; i <= rows; i++)
             {
-                string temporaryStorage = worksheet.Cells[i, 1].Text.Replace(":","");
+                string temporaryStorage = worksheet.Cells[i, 1].Text.Replace(":", "");
                 bool folderExist = folders.Any(folder => Path.GetFileName(folder) == temporaryStorage);
                 if (folderExist)
                 {
-                    worksheet.Cells[i, 1].Hyperlink = new ExcelHyperLink(fullFolderPath[i-1]);
+                    worksheet.Cells[i, 1].Hyperlink = new ExcelHyperLink(fullFolderPath[i - 1]);
                     worksheet.Cells[i, 1].Style.Font.UnderLine = true;
                     worksheet.Cells[i, 1].Style.Font.Color.SetColor(Color.Blue);
                 }
@@ -104,11 +104,34 @@ namespace KadastrAndHyperlinks
             package.Save();
         }
 
-        public ICommand GetLinks
+
+        public void WorkWithFileNew()
         {
-            get
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            string xlsxPath = pathToXlsx;
+            string rootFolder = pathToFolder;
+            var package = new ExcelPackage(xlsxPath);
+            var worksheet = package.Workbook.Worksheets[0];
+            int row = worksheet.Dimension.Rows;
+            int column = worksheet.Dimension.Columns;
+            string[] foldersName = Directory.GetDirectories(rootFolder);
+            
+            for (int columns = 1; columns <= column; columns++)
+        {
+                for (int rows = 1; rows <= row; rows++)
             {
-                return new RelayCommand(WorkWithFile);
+                    string temp = worksheet.Cells[rows, columns].Text.Replace(":", "");
+                    for (int i = 0; i < foldersName.Length; i++)
+                    {
+                        if (temp == Path.GetFileName(foldersName[i]))
+                        {
+                            string fullFolderPath = Path.Combine(rootFolder, foldersName[i]);
+                            worksheet.Cells[rows, columns].Hyperlink = new ExcelHyperLink(fullFolderPath);
+                            worksheet.Cells[rows, columns].Style.Font.UnderLine = true;
+                            worksheet.Cells[rows, columns].Style.Font.Color.SetColor(Color.Blue);
+                        }
+                    }
+                }
             }
         }
 
